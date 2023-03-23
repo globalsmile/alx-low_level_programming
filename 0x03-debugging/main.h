3421c48d33c6fdc43c67c3980705903611f1b1f4
globalsmile <mohammedbadmus823@gmail.com>
2023-03-23 16:41:24 -0700

    Fixed: Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,12 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdio.h>
 
+/*
+ * print_alphabet - function to print positive or negative number
+ *
+ */
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
