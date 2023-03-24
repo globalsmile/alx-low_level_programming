ea5cbd3070c57e61f5ecf927492cd2da4a7f879a
globalsmile <mohammedbadmus823@gmail.com>
2023-03-23 19:59:19 -0700

    Added: Task 0-2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,9 +7,8 @@
 
 int main(void)
 {
-        int i;
-
-        printf("Infinite loop incoming :(\n");
+	int i;
+	printf("Infinite loop incoming :(\n");
       /**  
        * i = 0;
        *
@@ -18,7 +17,6 @@ int main(void)
                 putchar(i);
        * }
        */
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
