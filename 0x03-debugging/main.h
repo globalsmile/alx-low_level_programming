d3c848b9609d9d47c9ed8ea3bce0207c8c22cc8f
globalsmile <mohammedbadmus823@gmail.com>
2023-03-23 18:38:47 -0700

    Added: Task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,18 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+/**
+ * print_alphabet - function to print alphabet
+ *
+ */
 void positive_or_negative(int i);
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
