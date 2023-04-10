0a19a664a99f26907ec40f2cfadd96088e2d15e5
globalsmile <mohammedbadmus823@gmail.com>
2023-04-10 16:09:21 -0700

    Added: Task 0

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - sets memory to array
+ * @s: array to set
+ * @b: value to set it as
+ * @n: n amount of times
+ * Return: char value of s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int k;
+
+	for (k = 0; k < n; k++)
+	{
+		s[k] = b;
+	}
+	return (s);
+}
