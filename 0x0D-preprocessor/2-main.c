49e1dd7b95ce04ae562d2e8fadbce4191ecba9f5
globalsmile <mohammedbadmus823@gmail.com>
2023-04-17 18:52:26 -0700

    Added: All Tasks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point, returns file name
+ * Return: returns 0 always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
