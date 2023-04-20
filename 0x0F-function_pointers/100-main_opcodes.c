87fdeb1fdbf103bb01573fe4ef2d800cc3cc3c03
globalsmile <mohammedbadmus823@gmail.com>
2023-04-19 18:48:25 -0700

    Fixed: Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,40 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <udis86.h>
-
 /**
-  * main - ...
-  * @argc: ...
-  * @argv: ...
-  *
-  * Return: ...
-  */
+ * main - entry point
+ * @argc: count of arguments
+ * @argv: array of argc
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
-	ud_t ud_obj;
-	int val = 0, i = 0;
+	int byte, num;
+	int (*addr)(int, char **) = main;
+	unsigned char opc;
 
-	if (argc == 2)
+	if (argc != 2)
 	{
-		val = atoi(argv[1]);
-
-		if (val < 0)
-		{
-			printf("Error\n");
-			exit(2);
-		}
-
-		ud_unit(&ud_obj);
-		ud_set_input_buffer(&ud_obj, argv[1], val);
-		ud_set_mode(&ud_obj, 64);
-		ud_set_syntax(&ud_obj, UD_SYN_INTEL);
-
-		while (ud_disassemble(&ud_obj))
-		{
-			printf("\t%s\n", ud_insn_hex(&ud_obj));
-		}
+		printf("Error\n");
+		exit(1);
 	}
+
+	byte = atoi(argv[1]);
+
+	if (byte < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (num = 0; num < byte; num++)
+	{
+		opc = *(unsigned char *)addr;
+		printf("%.2x",opc);
+
+		if (num == byte - 1)
+			continue;
+		printf(" ");
+
+		addr++;
+	}
+
+	printf("\n");
 
 	return (0);
 }
-
