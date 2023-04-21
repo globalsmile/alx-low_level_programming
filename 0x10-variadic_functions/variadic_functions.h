915ad68f2e8460deb104788beece36dcc13269d5
globalsmile <mohammedbadmus823@gmail.com>
2023-04-20 21:51:54 -0700

    Fixed: Task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,23 +1,18 @@
 #ifndef _HEADER_
 #define _HEADER_
 #include <stdarg.h>
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-int print_c(va_list a);
-int print_i(va_list a);
-int print_f(va_list a);
-int print_s(va_list a);
-/**
- * struct op - struct for 3
- * @c: character to check
- * @f: function to point to
- */
-typedef struct op
+void _printchar(va_list list);
+void _printstr(va_list list);
+void _printfloat(va_list list);
+void _printint(va_list list);
+
+typedef struct checker
 {
-	char *c;
-	int (*f)(va_list a);
-} printer;
+	char *type;
+	void (*f)();
+} checker;
 #endif
