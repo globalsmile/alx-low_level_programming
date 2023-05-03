8194f93d7d8b02b985321f9d5b34e902230a4fa3
globalsmile <mohammedbadmus823@gmail.com>
2023-05-02 17:46:12 -0700

    Added: 4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - free a `listint_t` list
+ * @head: head of linked list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *current_node, *next_node;
+	current_node = head;
+
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node);
+		current_node = next_node;
+	}
+}
