55204afb412ce6b332cf31c691ceaa69896ed9fd
globalsmile <mohammedbadmus823@gmail.com>
2023-05-02 19:18:17 -0700

    Added: fixed betty style

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,7 +8,7 @@ int sum_listint(listint_t *head)
 {
 	int sum_of_ints;
 	listint_t *current_node;
-	
+
 	for (sum_of_ints = 0, current_node = head; current_node != NULL;
 			current_node = current_node->next)
 		sum_of_ints += current_node->n;
