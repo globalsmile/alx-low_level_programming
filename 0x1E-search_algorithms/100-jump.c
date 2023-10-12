8d40e8d9eb9d702629b241872e0d952a578876e4
globalsmile <mohammedbadmus823@gmail.com>
2023-10-11 19:51:27 -0700

    update: 100-jump.c file

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 #include <math.h>
+#include "search_algos.h"
 #include "0-linear.c"
 
+/**
+  * jump_search - Searches for a value in an array of
+  * integers using the Linear search algorithm
+  *
+  * @array: A pointer to the first element of the array to search in
+  * @size: The number of elements in array
+  * @value: The value to search for
+  *
+  * Return: The first index where value is located
+  */
 int jump_search(int *array, size_t size, int value)
 {
 	size_t jump_step = sqrt(size);
